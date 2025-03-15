19013659aabfa10f3bc29225029252d35cc2297b
Upamanyu Sharma <upamanyu@mit.edu>
2025-03-15 12:00:11 -0400

    Try to add mef printing

--- mef.cbl
+++ mef.cbl
@@ -1,4 +1,4 @@
-           01 MER.
-              02 MER-SSN PIC 999999999.
-              02 MER-YEAR PIC 9999.
-              02 MER-AMNT PIC z(9)9.99.
+01 MER.
+  02 MER-SSN PIC 999999999.
+  02 MER-YEAR PIC 9999.
+  02 MER-AMNT PIC z(9)9.99.
