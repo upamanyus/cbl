79fa586fa7869fe22a2a6c7eb14df8f3b272c37e
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-03-15 16:25:24 -0400

    accumulate per-ssn

--- mef/sum.cbl
+++ mef/sum.cbl
@@ -42,32 +42,43 @@ LOCAL-STORAGE SECTION.
   02 filler PIC X VALUE " ".
   02 mer-year PIC 9(4).
   02 filler PIC X VALUE " ".
-  02 mer-amnt PIC 9(12).99 VALUE zero.
+  02 mer-amnt PIC 9(12).99.
 01 last-ssn PIC 9(9).
 01 mer-amnt-dec PIC 9(12)V99.
 01 total-income PIC 9(12)V99 VALUE zero.
-01 b_eof PIC A(1) VALUE 'F'.
+01 is-first PIC A VALUE 'T'.
+01 b_eof PIC A VALUE 'F'.
 
 PROCEDURE DIVISION.
 
 SORT work-mef ON ASCENDING KEY f_mer-ssn
 USING mef GIVING sorted-mef.
-OPEN INPUT sorted-mef.
 
+OPEN INPUT sorted-mef.
 PERFORM UNTIL b_eof = 'T'
   READ sorted-mef INTO mer
     AT END MOVE 'T' TO b_eof
+    NOT AT END
+      MOVE mer-amnt TO mer-amnt-dec
+
+      IF is-first='T' THEN
+        MOVE mer-ssn TO last-ssn
+        MOVE 'F' TO is-first
+      END-IF
+
+      IF mer-ssn=last-ssn THEN
+        ADD mer-amnt-dec TO total-income
+      ELSE
+        DISPLAY last-ssn
+        DISPLAY total-income
+        MOVE mer-ssn TO last-ssn
+        MOVE mer-amnt-dec TO total-income
+      END-IF
   END-READ
-
-  DISPLAY mer-ssn
-  DISPLAY mer-year
-  DISPLAY mer-amnt
-  MOVE mer-amnt TO mer-amnt-dec
-  ADD mer-amnt-dec TO total-income
-  DISPLAY mer-amnt-dec
-  DISPLAY total-income
 END-PERFORM.
-
 CLOSE sorted-mef.
+
+DISPLAY last-ssn
+DISPLAY total-income
 
 STOP RUN.
