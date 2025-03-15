38a181e73c67284308498fd1995622873bc933a3
Upamanyu Sharma <upamanyu@mit.edu>
2025-03-15 12:07:56 -0400

    Add build script; print works from root directory

---
+++ build.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from os import system as do
+
+programs = [
+    "mef/print",
+]
+
+do("mkdir -p ./bin/mef")
+
+for program_name in programs:
+    print(f"START building {program_name}.cbl")
+    print(f"DONE  at ./bin/{program_name}.")
+    do(f"cobc -O -free -x {program_name}.cbl -o ./bin/{program_name}")
